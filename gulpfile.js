3eaa2dc2f1b341d6ff6b76177d24034f18a0a783
Cameron Messinides <cameron.messinides@gmail.com>
2017-04-07 10:27:18 -0400

    Add docs to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sassdoc = require('sassdoc');
 
 const sassPath = 'scss/**/*.scss';
 
@@ -8,6 +9,16 @@ gulp.task('sass', function() {
       .pipe(sass())
       .pipe(gulp.dest('css'));
 })
+
+gulp.task('docs', function() {
+  return gulp.src(sassPath)
+      .pipe(sassdoc({
+        dest: 'site/docs',
+        verbose: true
+      }))
+})
+
+gulp.task('default', ['sass', 'docs'])
 
 gulp.task('watch', ['sass'], function() {
   gulp.watch(sassPath, ['sass']);
