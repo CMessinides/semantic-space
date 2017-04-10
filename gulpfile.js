b01aea69bddd4f7263d821dcfabd771bc8d9262b
Cameron Messinides <cameron.messinides@gmail.com>
2017-04-10 16:46:44 -0400

    Add a minify step to the build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    sassdoc = require('sassdoc');
+    sassdoc = require('sassdoc'),
+    rename = require('gulp-rename');
 
 const sassPath = 'scss/**/*.scss';
 
 gulp.task('sass', function() {
   return gulp.src(sassPath)
-      .pipe(sass())
+      .pipe(sass({outputStyle:'expanded'}))
+      .pipe(gulp.dest('css'));
+})
+
+gulp.task('minify', function() {
+  return gulp.src('css/*.css')
+      .pipe(sass({outputStyle:'compressed'}))
+      .pipe(rename(function(path) {
+        path.extname = ".min.css";
+      }))
       .pipe(gulp.dest('css'));
 })
 
@@ -18,8 +28,9 @@ gulp.task('docs', function() {
       }))
 })
 
-gulp.task('default', ['sass', 'docs'])
+gulp.task('default', ['sass', 'minify', 'docs'])
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['sass', 'minify'], function() {
   gulp.watch(sassPath, ['sass']);
+  gulp.watch('css/*.css', ['minify']);
 })
